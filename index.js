2ec73fc7886e813c6cec02691b3a10ab7d446a1e
lucasronnye <lucasronnye@gmail.com>
2024-10-26 03:22:18 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// variavel com o nome e a quantidade de experiência(XP) do herói
+let nomeHeroi = "Capitão América";
+let xpHeroi = 4520;
+//Estrutura de decisão Switch Case
+if(xpHeroi < 1000){
+  console.log("O Herói de nome " + nomeHeroi +" está no nível de Ferro");
+}
+else if(xpHeroi >= 1001 && xpHeroi <= 2000){
+  console.log("O Herói de nome " + nomeHeroi +" está no nível de Bronze");
+}
+else if(xpHeroi >= 2001 && xpHeroi <= 5000){
+  console.log("O Herói de nome " + nomeHeroi +" está no nível de Prata");
+}
+else if(xpHeroi >= 5001 && xpHeroi <= 7000){
+  console.log("O Herói de nome " + nomeHeroi +" está no nível de Ouro");
+}
+else if(xpHeroi >= 7001 && xpHeroi <= 8000) {
+  console.log("O Herói de nome " + nomeHeroi +" está no nível de Platina");
+}
+else if(xpHeroi >= 8001 && xpHeroi <= 9000) {
+  console.log("O Herói de nome " + nomeHeroi +" está no nível de Ascendente");
+}
+else if(xpHeroi >= 9001 && xpHeroi <= 10000) {
+  console.log("O Herói de nome " + nomeHeroi +" está no nível de Imortal");
+}
+else if(xpHeroi>= 10001){
+  console.log("O Herói de nome " + nomeHeroi +" está no nível de Radiante");
+}
+          
